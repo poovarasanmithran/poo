f745f4ace41e8479cb22310c6bff3025c64d6809
poovarasanmithran <31285551+poovarasanmithran@users.noreply.github.com>
2018-01-31 16:02:38 +0530

    factorial.c
    to print thr factorial of an number

---
+++ factorial.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include<string.h>
+void main()
+{
+    char j=1,i,n;
+    clrscr();
+    scanf("%d",&n\n);
+    for(i=1;i<=n;i++) j *= i;
+    printf("%d",j);
+    return 0;
+}
